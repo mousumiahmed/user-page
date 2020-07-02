8cfd1c8cbdc4a22de48d7e7a1f0ff4e6bc8c33e1
mousumiahmed <mousumiahmed786@gmail.com>
2020-07-02 13:50:48 +0530

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ export default class NavBar extends Component {
             <div>
                  <AppBar  position="static" style={{ backgroundImage: "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)"}}>
                     <Toolbar >
+                    USER PAGE
                     </Toolbar>
                 </AppBar>
             </div>
